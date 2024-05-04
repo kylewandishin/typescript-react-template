59e86197dfbdfbd4a57ac0f7cd67bcb482943387
Kyle W <73055576+dishman1805@users.noreply.github.com>
2024-05-04 16:50:04 -0600

    add sample 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
       {/* <Route path="/about" element={<About />} /> */}
     </Routes>
   );
