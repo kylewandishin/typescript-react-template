cc91511927ee4813b7ad60b844735e1a600c6423
Kyle W <73055576+dishman1805@users.noreply.github.com>
2024-05-04 16:21:27 -0600

    add routing and sample tests

---
+++ src/App.test.tsx
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+
+import App from './App';
+
+describe('App', () => {
+  it('renders Hello, World!', () => {
+    render(<App />);
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      }),
+    ).toHaveTextContent('Hello, World!');
+  });
+});
