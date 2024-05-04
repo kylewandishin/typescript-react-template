19e9c438d8f3b97835891e8fb1e903d875cd8fbe
Kyle W <73055576+dishman1805@users.noreply.github.com>
2024-05-04 14:12:43 -0600

    basic vite app + eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,57 @@
+module.exports = {
+  ignorePatterns: ["lib/**", "**/*.js"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "tsdoc"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "prettier",
+  ],
+  parserOptions: {
+    project: ["./tsconfig.json"],
+  },
+  rules: {
+    "@typescript-eslint/explicit-member-accessibility": [
+      "error",
+      {
+        accessibility: "explicit",
+        overrides: {
+          accessors: "explicit",
+          constructors: "explicit",
+          methods: "explicit",
+          properties: "explicit",
+          parameterProperties: "explicit",
+        },
+      },
+    ],
+    "no-shadow": "error",
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "variable",
+        types: ["boolean"],
+        format: ["PascalCase", "UPPER_CASE"],
+        prefix: [
+          "is",
+          "should",
+          "has",
+          "can",
+          "did",
+          "will",
+          "does",
+          "IS",
+          "SHOULD",
+          "HAS",
+        ],
+      },
+      {
+        selector: "typeLike",
+        format: ["PascalCase"],
+      },
+    ],
+    "@typescript-eslint/array-type": ["error", { default: "generic" }],
+    "new-cap": "error",
+    "tsdoc/syntax": "error",
+  },
+};
